ab48e5226c7c026da6cfc8fedf3e91d297e80bc0
mb <mb150996@gmail.com>
2025-06-24 15:59:20 +0900

    diablo: Declare config_num_physical_slots
    Change-Id: I95edd27ecd194cc886c67c74ebf5ba1727c40e8a

--- device.mk
+++ device.mk
@@ -16,6 +16,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/resourcemanager_waipio_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/resourcemanager_waipio_mtp.xml \
     $(LOCAL_PATH)/configs/audio/usecaseKvManager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usecaseKvManager.xml
 
+# Overlay
+PRODUCT_PACKAGES += \
+    FrameworkResDiablo
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
