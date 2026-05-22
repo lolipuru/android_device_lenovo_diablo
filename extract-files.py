95645c1b24a533819c648db2f7106e006ed68b25
lolipuru <banstellazz@gmail.com>
2026-05-22 01:36:19 +0000

    diablo: initial proprietary-files
    loosely based from halo, they're closed enough
    
    Change-Id: I6f35b28ed96838108918e1e722b7e27f468432cd
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -27,6 +27,44 @@ namespace_imports = [
     'vendor/lenovo/sm8475-common',
 ]
 
+blob_fixups: blob_fixups_user_type = {
+    (
+        'vendor/bin/hw/android.hardware.gnss-aidl-service-qti',
+        'vendor/lib64/hw/android.hardware.gnss-aidl-impl-qti.so',
+        'vendor/lib64/libgarden.so',
+        'vendor/lib64/libgarden_haltests_e2e.so',
+    ): blob_fixup()
+        .replace_needed(
+            'android.hardware.gnss-V1-ndk_platform.so',
+            'android.hardware.gnss-V1-ndk.so',
+        ),
+    'vendor/bin/hw/vendor.qti.hardware.display.composer-service': blob_fixup()
+        .replace_needed('android.hardware.common-V2-ndk_platform.so', 'android.hardware.common-V2-ndk.so')
+        .replace_needed('vendor.qti.hardware.display.config-V4-ndk_platform.so', 'vendor.qti.hardware.display.config-V5-ndk.so'),
+    'vendor/lib64/libcamximageformatutils.so': blob_fixup()
+        .replace_needed(
+            'vendor.qti.hardware.display.config-V2-ndk_platform.so',
+            'vendor.qti.hardware.display.config-V2-ndk.so',
+        ),
+    (
+        'vendor/lib64/libpandora.render.so',
+    ): blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_lockPlanes')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
+}
+
+module = ExtractUtilsModule(
+    'diablo',
+    'lenovo',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    check_elf=True,
+)
+
 if __name__ == '__main__':
     utils = ExtractUtils.device(module)
     utils.run()
